6df709fa3cca3dbd9c4532a141fd3cce34d9140f
asarahre <sarahbraga3433@gmail.com>
2022-01-03 13:44:45 -0400

    segundo commit

--- App.tsx
+++ App.tsx
@@ -4,6 +4,5 @@ import { SignIn } from './src/screens/Signin';
 export default function App() {
   return (
     <SignIn />
-
   );
 }
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { Image, StatusBar, Text, View } from 'react-native';
+import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+3
 export function SignIn() {
-    const [text, setText] = useState('')
+    // const [text, setText] = useState('')
     return (
         <View style={styles.container}>
-            <Text>Hello World, 2022 </Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setText} />
-            <text>
-                Você digitou: {text}
-            </text>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparert"
+                translucent
+            />
+            <Image source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {'\n'}
+                    suas jogatinas {'\n'}
+                    facilmente  {'\n'}
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+            </View>
         </View>
     );
 }
