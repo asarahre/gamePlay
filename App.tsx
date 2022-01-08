77670a838ea0b78f855d932212eedb759fc4692d
asarahre <sarahbraga3433@gmail.com>
2022-01-08 12:10:59 -0400

    feat: routes

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
-
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
+
 
 export default function App() {
 
@@ -19,13 +19,13 @@ export default function App() {
     return <AppLoading />
   }
   return (
-    <Background >
+    <>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent={true}
       />
-      <Home />
-    </Background>
+      <Routes />
+    </>
   );
 }
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Home } from "../screens/Home";
+import { SignIn } from "../screens/Signin";
+import { Background } from "../components/Background";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+
+    return (
+
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: { //Does not work,
+                    backgroundColor: 'transparent',
+                },
+            }}>
+
+            <Screen
+                name="SignIn"
+                component={SignIn}
+            />
+            <Screen
+                name="Home"
+                component={Home}
+            />
+
+        </Navigator>
+
+
+    );
+
+}
+
